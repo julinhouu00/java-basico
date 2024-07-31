9fdb34178280ced7b18546357e3884d1c1dd6d0f
julinhouu00 <138164602+julinhouu00@users.noreply.github.com>
2024-07-31 19:41:46 -0300

    new commit

--- controle-de-fluxo/src/ResultadosEscolar.java
+++ controle-de-fluxo/src/ResultadosEscolar.java
@@ -1,13 +1,13 @@
 // ResultadoEscolar.java
 public class ResultadosEscolar {
     public static void main(String[] args) {
+        int nota = 6;
 
-       int nota = 6;
-       
-       if(nota >= 7)
-        System.out.println("Aprovado");
-
-       else
-        System.out.println("Reprovado");
+	if (nota >= 7)
+		System.out.println("Aprovado");
+	else if (nota >= 5 && nota < 7)
+		System.out.println("Recuperação");
+	else
+		System.out.println("Reprovado");
     }
 }
